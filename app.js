9d62b9f26879b612256832b0544cf29a31ab4d3f
Katherine Day <kmday125@gmail.com>
2024-09-06 15:06:13 +0100

    first push with basic set up

---
+++ app.js
@@ -0,0 +1,27 @@
+console.log("test");
+
+let images = [
+  {
+    src: "https://pixabay.com/photos/teamwork-cooperation-brainstorming-3213924/", //put these in an assets folder
+    alt: "test image",
+    height: 200,
+    width: 300,
+  },
+  {
+    src: "https://pixabay.com/photos/teamwork-cooperation-brainstorming-3213924/",
+    alt: "test2",
+    height: 200,
+    width: 300,
+  },
+];
+
+//STEP 1: select the DOM elements that will contain my images
+//select thubnail conatiner
+//select the main image container
+
+//STEP 2: write a function to create the images in our thumbnail and append them later
+function createThumbnails(thumbnailContainer) {
+  thumbnailContainer.forEach(function (thumbnail, index) {});
+}
+
+function createLargeImage() {}
