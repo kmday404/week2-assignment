0f97a2db5e3ecaab32cc35e3307a7196391346af
Katherine Day <kmday125@gmail.com>
2024-09-07 10:34:37 +0100

    array created with images on JS

--- app.js
+++ app.js
@@ -2,19 +2,45 @@ console.log("test");
 
 let images = [
   {
-    src: "", //put these in an assets folder
-    alt: "test image",
+    src: "./assets/car-interior.jpg",
+    alt: "Car interior",
     height: 200,
     width: 300,
   },
   {
-    src: "",
-    alt: "test2",
+    src: "./assets/cat-1.jpg",
+    alt: "Cat",
+    height: 200,
+    width: 300,
+  },
+  {
+    src: "./assets/cygnet.jpg",
+    alt: "Cygnet",
+    height: 200,
+    width: 300,
+  },
+  {
+    src: "./assets/red-arrows.jpg",
+    alt: "The red arows (planes)",
+    height: 200,
+    width: 300,
+  },
+  {
+    src: "./assets/sky-mist.jpg",
+    alt: "Orange sky over mountains with mist covering the hills",
+    height: 200,
+    width: 300,
+  },
+  {
+    src: "./assets/strawberries.jpg",
+    alt: "Strawberries",
     height: 200,
     width: 300,
   },
 ];
 
+const thumbnailContainer = document.getElementById("thumbnail-container");
+console.log(thumbnailContainer);
 //STEP 1: select the DOM elements that will contain my images
 //select thubnail conatiner
 //select the main image container
